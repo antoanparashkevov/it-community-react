1e219f8b81302a967f57e55dd1602d2106fcfbd6
“antoanparashkevov” <tonkata1505@gmail.com>
2023-01-28 15:46:18 +0200

    Register the first route with the react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import React from "react";
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 //Components
 import TheHeader from "./components/UI/TheHeader";
 
+//create a relation between the routes and the components
+//or simply we register our routes here
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <TheHeader/>
+  },
+  {
+    
+  },
+])
+
 function App() {
   return (
-      <TheHeader/>
+      <RouterProvider router={routes}/>
   );
 }
 
