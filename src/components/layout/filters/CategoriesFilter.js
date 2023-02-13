5bb87210d38c3b1258fbb74b8abd15a087cda576
“antoanparashkevov” <tonkata1505@gmail.com>
2023-02-13 19:09:16 +0200

    Custom input field

--- src/components/layout/filters/CategoriesFilter.js
+++ src/components/layout/filters/CategoriesFilter.js
@@ -7,22 +7,22 @@ const CategoriesFilter = () => {
             <div className={styles['categories_form_controls']}>
                 <div className={styles['form_control']}>
                     <label htmlFor="frontend">Frontend</label>
-                    <input type="checkbox" id='frontend' name='frontend' value='frontend'/>
+                    <input type="checkbox" id='frontend' name='frontend' value='frontend' defaultChecked={true}/>
                     <span className={styles['checkmark']}></span>
                 </div>
                 <div className={styles['form_control']}>
                     <label htmlFor="backend">Backend</label>
-                    <input type="checkbox" id='backend' name='backend' value='backend' checked/>
+                    <input type="checkbox" id='backend' name='backend' value='backend' defaultChecked={true}/>
                     <span className={styles['checkmark']}></span>
                 </div>
                 <div className={styles['form_control']}>
                     <label htmlFor="qa">Quality Assurance</label>
-                    <input type="checkbox" id='qa' name='qa' value='qa' checked/>
+                    <input type="checkbox" id='qa' name='qa' value='qa' defaultChecked={true}/>
                     <span className={styles['checkmark']}></span>
                 </div>
                 <div className={styles['form_control']}>
                     <label htmlFor="infrastructure">Infrastructure</label>
-                    <input type="checkbox" id='infrastructure' value='infrastructure' checked/>
+                    <input type="checkbox" id='infrastructure' value='infrastructure' defaultChecked={true}/>
                     <span className={styles['checkmark']}></span>
                 </div>
             </div>
