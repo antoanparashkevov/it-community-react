f32a54d498cf3dd1f62dc3baa5b95750f4ab80ea
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-06 16:48:37 +0200

    Moves the routes into a separate file

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,8 @@
 import './App.css';
 import React from "react";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-//Components
-import PostersList, { loader as postersLoader } from "./components/pages/applying/PostersList";
-import Companies from "./components/pages/companies/Companies";
-import Messages from "./components/pages/messages/Messages";
-import Board from "./components/pages/applying/Board";
-import RootLayout from "./components/Root";
-import ErrorPage from "./components/pages/Error";
-import PosterDetails, { loader as posterDetailsLoader } from "./components/pages/applying/PosterDetails";
-import Applying from "./components/pages/applying/Applying";
-import UserAuth from "./components/pages/auth/UserAuth";
-
-//create a relation between the routes and the components,
-//or simply we register our routes here
-
-const routes = createBrowserRouter([
-    { 
-        path: '/',
-        element: <RootLayout/>,
-        errorElement: <ErrorPage/>,//this route will be triggered whenever a loader throws an Error or when a user visits wrong URL
-        children: [
-            {
-                // or path: ''
-                index: true,//it's the default route that should be display if the root path is active (/)
-                element: <Board/>
-            },
-            {
-                path: 'posters',
-                id: 'posters',
-                children: [
-                    {
-                        index: true,
-                        element: <PostersList/>,
-                        loader: postersLoader,
-                    },
-                    {
-                        path: ':posterId',
-                        id: 'poster-details',
-                        loader: posterDetailsLoader,
-                        children: [
-                            {
-                               index: true,
-                               element: <PosterDetails/>,
-                            },
-                            {
-                                path: '',
-                                element: <PosterDetails/>,
-                                children: [
-                                    {
-                                        path: 'apply',
-                                        element: <Applying/>
-                                    }
-                                ]
-                            },
-                        ],
-                    },
-                ]
-            },
-            {
-                path: 'companies',
-                element: <Companies/>
-            },
-            {
-                path: 'messages',
-                element: <Messages/>
-            },
-            {
-                path: 'auth',
-                element: <UserAuth/>
-            }
-        ]
-    }
-]);
+import { RouterProvider } from 'react-router-dom';
+import { routes } from "./util/routes";
     
 function App() {
       //register the list of routes
---
+++ client/src/util/routes.js
@@ -0,0 +1,80 @@
+import { createBrowserRouter } from "react-router-dom";
+import React from "react";
+
+//components
+import RootLayout from "../components/Root";
+import ErrorPage from "../components/pages/Error";
+import Board from "../components/pages/applying/Board";
+import PostersList, { loader as postersLoader } from "../components/pages/applying/PostersList";
+import PosterDetails, { loader as posterDetailsLoader } from "../components/pages/applying/PosterDetails";
+import Applying from "../components/pages/applying/Applying";
+import Companies from "../components/pages/companies/Companies";
+import Messages from "../components/pages/messages/Messages";
+import UserAuth from "../components/pages/auth/UserAuth";
+import CreateJob from "../components/pages/applying/CreateJob";
+
+//create a relation between the routes and the components,
+//or simply we register our routes here
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout/>,
+        errorElement: <ErrorPage/>,//this route will be triggered whenever a loader throws an Error or when a user visits wrong URL
+        children: [
+            {
+                // or path: ''
+                index: true,//it's the default route that should be display if the root path is active (/)
+                element: <Board/>
+            },
+            {
+                path: 'posters',
+                id: 'posters',
+                children: [
+                    {
+                        index: true,
+                        element: <PostersList/>,
+                        loader: postersLoader,
+                    },
+                    {
+                        path: ':posterId',
+                        id: 'poster-details',
+                        loader: posterDetailsLoader,
+                        children: [
+                            {
+                                index: true,
+                                element: <PosterDetails/>,
+                            },
+                            {
+                                path: '',
+                                element: <PosterDetails/>,
+                                children: [
+                                    {
+                                        path: 'apply',
+                                        element: <Applying/>
+                                    }
+                                ]
+                            },
+                        ],
+                    },
+                ]
+            },
+            {
+                path: 'companies',
+                element: <Companies/>
+            },
+            {
+                path: 'messages',
+                element: <Messages/>
+            },
+            {
+                path: 'auth',
+                element: <UserAuth/>
+            },
+            {
+                path: 'create',
+                element: <CreateJob/>
+            }
+        ]
+    }
+]);
