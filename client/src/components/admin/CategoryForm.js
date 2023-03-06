6bddb4f000cff260716cac7a3ebb9bf968da98b7
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-06 22:13:15 +0200

    Category form and SubCategory form created + styles

--- client/src/components/admin/CategoryForm.js
+++ client/src/components/admin/CategoryForm.js
@@ -1,9 +1,69 @@
 import styles from './CategoryForm.module.scss'
+import useInput from "../../hooks/use-input";
+import { Input } from "../layout/Input";
+import { RoundedButton } from "../UI/BaseButton";
 
-const CategoryForm = () => {
+const CategoryForm = ({ children }) => {
+    let formIsValid;
+
+    const {
+        value : enteredCategoryName,
+        isValid : categoryNameInputIsValid,
+        hasError : categoryNameInputHasError,
+        reset : resetCategoryNameInput,
+        valueChangeHandler : categoryNameChangeHandler,
+        inputBlurHandler : categoryNameBlurHandler
+    } = useInput(value => value.trim() !== '' && value.trim().length >= 5);
+    
+
+    formIsValid = categoryNameInputIsValid
+
+    const formSubmissionHandler = (event) => {
+        //event param - event object describing the event
+
+        /*
+            the default behaviour is if the form is submitted,
+            a http req is sent to the server to the same address
+        */
+        event.preventDefault();
+
+        console.log('category name >>> ', enteredCategoryName);
+
+        resetCategoryNameInput();
+    }
+
+    const formControlClasses = (hasError) => {
+        return hasError ? `${styles['form-control']} invalid` : styles['form-control']
+    }
+
     return (
-        <h1>cat form</h1>
+        <form className={ styles['category_form'] } onSubmit={ formSubmissionHandler }>
+            <div className={ formControlClasses(categoryNameInputHasError) }>
+
+                {categoryNameInputHasError && <p>Please enter a valid non-empty category name and at least 5 characters long!</p>}
+                <label htmlFor="cat_name">Category name*</label>
+                <Input
+                    typeCat='cat_name'
+                    id='cat_name'
+                    name='cat_name'
+                    onChange={ categoryNameChangeHandler }
+                    onBlur={ categoryNameBlurHandler }
+                    value={ enteredCategoryName }
+                />
+            </div>
+
+            {children}
+            
+            <div className={ styles['form-actions'] }>
+                <RoundedButton
+                    disabled={ !formIsValid }
+                    type='submit'
+                >
+                    Create
+                </RoundedButton>
+            </div>
+        </form>
     )
 }
-
+    
 export default CategoryForm;
--- client/src/components/pages/admin/CreateSubCategory.js
+++ client/src/components/pages/admin/CreateSubCategory.js
@@ -1,9 +1,21 @@
+
+//UI components
 import FormPageContent from "../../UI/FormPageContent";
+import CategoryForm from "../../admin/CategoryForm";
+import DataSelectorWrapper from "../../UI/DataSelectorWrapper";
 
 const CreateSubCategory = () => {
+    const categories = [
+        {
+            code: 'frontend',
+            displayName: 'Frontend'
+        }
+    ]
     return (
         <FormPageContent title='Create a sub category'>
-            
+            <CategoryForm>
+                <DataSelectorWrapper closeOnHover selectorData={categories} initialPlaceholderValue={categories[0].displayName}/>
+            </CategoryForm>
         </FormPageContent>
     )   
 }
