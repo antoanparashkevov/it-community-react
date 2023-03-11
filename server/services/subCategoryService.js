e7cb5313326ea7209509782a6a53e60597409ae4
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-11 10:42:31 +0200

    Processes getById in the SubcategoryService

--- server/services/subCategoryService.js
+++ server/services/subCategoryService.js
@@ -4,9 +4,15 @@ async function getAll() {
     return SubCategory.find({})
 }
 
+async function getById(id) {
+    return SubCategory.findById(id)
+}
+
 async function getByCode(code) {
     return SubCategory.find({ code: code })
 }
+
+// async function getByCodeAndUpdate(code, updateField)
 
 async function create(item) {
     return SubCategory.create(item)
@@ -15,6 +21,7 @@ async function create(item) {
 
 module.exports = {
     getAll,
+    getById,
     getByCode,
     create
 }
