411359903c7ebd30837f25ab056ffa9ea3cf00bb
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-13 16:02:00 +0200

    handle all form fields for authorization and send a post request to the server

--- client/src/components/auth/AuthForm.js
+++ client/src/components/auth/AuthForm.js
@@ -1,6 +1,6 @@
 import styles from './AuthForm.module.scss';
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 //UI components
 import { Input } from "../layout/Input";
@@ -9,9 +9,16 @@ import Label from "../UI/Label";
 
 //hooks
 import useInput from "../../hooks/use-input";
+import useHttp from "../../hooks/use-http";
+import BaseDialog from "../UI/BaseDialog";
+import BaseSpinner from "../UI/BaseSpinner";
 
 const AuthForm = ( { authMode } ) => {
     let formIsValid;
+    
+    const { isLoading, error, resetError, setAdditionalErrors, sendRequest } = useHttp();
+    const [authResponse, setAuthResponse] = useState(null);
+    const navigation = useNavigate();
     
     const {
         value : enteredEmail,
@@ -29,11 +36,11 @@ const AuthForm = ( { authMode } ) => {
         reset : resetPasswordInput,
         valueChangeHandler : passwordChangeHandler,
         inputBlurHandler : passwordBlurHandler
-    } = useInput(value => value.trim() !== '');
+    } = useInput(value => value.trim() !== '' && value.trim().length >= 6);
     
     formIsValid = emailInputIsValid && passwordInputIsValid
 
-    const formSubmissionHandler = (event) => {
+    const formSubmissionHandler = async (event) => {
         //event param - event object describing the event
 
         /*
@@ -45,8 +52,29 @@ const AuthForm = ( { authMode } ) => {
         console.log('email >>> ', enteredEmail);
         console.log('password >>> ', enteredPassword);
 
-        resetEmailInput();
+        
+        
+       if(authMode !== 'login' || authMode !== 'signup') {
+           setAdditionalErrors('Invalid authentication mode!')
+       }
+       
+       await sendRequest('/authData/' + authMode, 'POST', (data) => setAuthResponse(data), {
+           email: enteredEmail,
+           password: enteredPassword,
+           role: 'user'
+       })
+
+        console.log('formIsValid', formIsValid)
+        console.log('error', !error)
+        
+        if( error || formIsValid === false ) {
+            return
+        }
+
         resetPasswordInput();
+        resetEmailInput();
+        navigation('/')
+        
     }
 
     const formControlClasses = (hasError) => {
@@ -57,52 +85,60 @@ const AuthForm = ( { authMode } ) => {
     const switchButtonCaption = authMode === 'login' ? 'Sign up instead' : 'Login instead'
     
     return (
-        <form className={ styles['apply_form'] } onSubmit={ formSubmissionHandler }>
-            <div className={ formControlClasses(emailInputHasError) }>
+        <React.Fragment>
+            {error && 
+                <BaseDialog fixed={false} show={!!error} title='Something went wrong with the authentication process' onCloseDialog={resetError}>
+                    {error}
+                </BaseDialog>
+            }
+            {isLoading && <BaseSpinner />}
+            <form className={ styles['apply_form'] } onSubmit={ formSubmissionHandler }>
+                <div className={ formControlClasses(emailInputHasError) }>
 
-                {emailInputHasError && <p>Please enter a valid non-empty name!</p>}
-                <Label for="email">Email*</Label>
-                <Input 
-                    typeCat='email' 
-                    id='email' 
-                    name='email'
-                    onChange={ emailChangeHandler }
-                    onBlur={ emailBlurHandler }
-                    value={ enteredEmail }
-                />
-            </div>
-            <div className={ formControlClasses(passwordInputHasError) }>
+                    {emailInputHasError && <p>Please enter a valid non-empty name!</p>}
+                    <Label for="email">Email*</Label>
+                    <Input
+                        typeCat='email'
+                        id='email'
+                        name='email'
+                        onChange={ emailChangeHandler }
+                        onBlur={ emailBlurHandler }
+                        value={ enteredEmail }
+                    />
+                </div>
+                <div className={ formControlClasses(passwordInputHasError) }>
 
-                {passwordInputHasError && <p>Please enter a non-empty password!</p>}
-                <Label for="pass">Password*</Label>
-                <Input 
-                    typeCat='password' 
-                    id='pass' 
-                    name='pass'
-                    onChange={ passwordChangeHandler }
-                    onBlur={ passwordBlurHandler }
-                    value={ enteredPassword }
-                    
-                />
-            </div>
-            <div className={ styles['form-actions'] }>
-                <Button 
-                    className={ styles['form-main-btn'] }
-                    disabled={ !formIsValid }
-                    type='submit'
-                >
-                    { submitButtonCaption }
-                </Button>
-                <OutlineButton 
-                    className={ styles['form-second-btn'] }
-                    type='button'
-                    as={Link}
-                    to={`?mode=${authMode === 'login' ? 'signup' : 'login'}`}
-                >
-                    { switchButtonCaption }
-                </OutlineButton>
-            </div>
-        </form>
+                    {passwordInputHasError && <p>Please enter a non-empty password with at least 6 characters long!</p>}
+                    <Label for="pass">Password*</Label>
+                    <Input
+                        typeCat='password'
+                        id='pass'
+                        name='pass'
+                        onChange={ passwordChangeHandler }
+                        onBlur={ passwordBlurHandler }
+                        value={ enteredPassword }
+
+                    />
+                </div>
+                <div className={ styles['form-actions'] }>
+                    <Button
+                        className={ styles['form-main-btn'] }
+                        disabled={ !formIsValid }
+                        type='submit'
+                    >
+                        { submitButtonCaption }
+                    </Button>
+                    <OutlineButton
+                        className={ styles['form-second-btn'] }
+                        type='button'
+                        as={Link}
+                        to={`?mode=${authMode === 'login' ? 'signup' : 'login'}`}
+                    >
+                        { switchButtonCaption }
+                    </OutlineButton>
+                </div>
+            </form>
+        </React.Fragment>  
     )
 }
 
