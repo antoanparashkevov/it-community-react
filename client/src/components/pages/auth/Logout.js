5e5f365f201ac5915a388ea1421e4f27a9c42e2c
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-13 18:08:11 +0200

    Send a request to logout

--- client/src/components/pages/auth/Logout.js
+++ client/src/components/pages/auth/Logout.js
@@ -1,6 +1,9 @@
 import { redirect } from "react-router-dom";
 
-export function action () {
+const host =  process.env.REACT_APP_DEFAULT_URL || 'http://localhost:3030';
+
+export async function action () {
+    await fetch(host + '/authData/logout')
     localStorage.clear();
 
     return redirect('/')
