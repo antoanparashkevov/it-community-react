6df81728b54a9f01af1dee58d13e35627b71e098
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-15 22:25:01 +0200

    When the token expires, submit the logout action and clear the localstorage

--- client/src/components/Root.js
+++ client/src/components/Root.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import { Outlet } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from "react";
+import { Outlet, useSubmit } from 'react-router-dom'
 import styled from "styled-components";
 
 //Components
 import TheHeader from "./layout/TheHeader";
 import Footer from "./layout/Footer";
+import { calculateExpirationDate, getAuthToken } from "../util/auth";
 
 export const HeaderWrapper = styled.header`
     width: 100%;
@@ -26,6 +27,34 @@ export const FooterWrapper = styled.footer`
 `
 
 const RootLayout = () => {
+    const token = getAuthToken()
+    const submit = useSubmit();
+    
+    
+    useEffect(() => {
+        if( !token ) {//it should be null if you are not authenticated.
+            return;
+        }
+        
+        if ( token === 'EXPIRED' ) {
+            submit(null, {
+                action: '/logout',
+                method: 'POST'
+            })
+        }
+
+        let tokenDuration = calculateExpirationDate();
+        
+        if( tokenDuration && tokenDuration > 0 ) {
+            setTimeout( () => {
+                submit(null, {
+                    action: '/logout',
+                    method: 'POST'
+                })
+            }, tokenDuration)//1h
+        }
+        
+    }, [token, submit])
     
     return (
         <React.Fragment>
