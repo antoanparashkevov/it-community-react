ce7e14e9220241ed370a3d864792ca9197d589e5
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-15 22:16:44 +0200

    Add error dialog when the user session expired

--- client/src/components/pages/applying/Board.js
+++ client/src/components/pages/applying/Board.js
@@ -6,33 +6,44 @@ import BoardItem from "../../applying/BoardItem";
 
 //hooks
 import useHttp from "../../../hooks/use-http";
+import BaseDialog from "../../UI/BaseDialog";
+import BaseSpinner from "../../UI/BaseSpinner";
+
+//token
+import { getAuthToken } from "../../../util/auth";
 
 const Board = () => {
     const [categories, setCategories] = useState([])
-    const {isLoading, error, sendRequest: fetchCategories} = useHttp()
+    const {isLoading, error, sendRequest: fetchCategories, resetError} = useHttp()
     
     useEffect(() => {
         fetchData()
     }, [])
     
-    const fetchData = useCallback(async () => {
+    const fetchData = async () => {
       await fetchCategories('/categoryData/categories','GET', transformCategoryData)
-    }, [])
+    }
     
     const transformCategoryData = (data) => {
-        setCategories(data)
+        setCategories(data.items)
     }
     
     return (
-          <section className={styles['homepage_main']}>
-              
-              <div className={`${styles['homepage_categories']}`}>
+        <React.Fragment>
+            {/* TODO FOR ALL ROUTES ?*/}
+            {error && <BaseDialog show={!!error} onCloseDialog={resetError} fixed={false} title={getAuthToken() === 'EXPIRED' ? 'Your session expired!' : 'Something went wrong with fetching the Categories!'}>{ getAuthToken() === 'EXPIRED' ? 'Reload the page and sign in again' : error }</BaseDialog>}
+            {/*TODO MAYBE REPLACE WITH A SKELETON LOADING*/}
+            {isLoading && <BaseSpinner />}
+            <section className={styles['homepage_main']}>
 
-                  { categories.map((category, index) => 
-                     <BoardItem key={index} category={category}/>
-                  )}
-              </div>
-          </section>
+                <div className={`${styles['homepage_categories']}`}>
+
+                    { categories.map((category, index) =>
+                        <BoardItem key={index} category={category}/>
+                    )}
+                </div>
+            </section>
+        </React.Fragment>
     )
 }
 
