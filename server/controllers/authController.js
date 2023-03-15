387e2d70e1c47303ba9b35b9d9762627d4f8cb8f
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-15 22:22:03 +0200

    Divide the authController into two pieces. One for the User and one for the Company

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
-const {register, login, logout, registerAsCompany } = require("../services/authService");
-const {body,validationResult} = require('express-validator');
+const {register, login, logout, registerAsCompany, loginAsCompany } = require("../services/authService");
+const { body,validationResult } = require('express-validator');
 const router = require('express').Router();
 
 const parseError = require('../util/parseError');
@@ -7,12 +7,23 @@ const parseError = require('../util/parseError');
 router.post('/signup',
     body('email').isEmail().withMessage('Invalid email'),
     body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long!'),
-    async (req,res)=> {
+    async (req,res) => {
     await authAction(req,res,'register',400)
+})
+
+router.post('/signup_company',
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long!'),
+    async (req,res) => {
+    await authAction(req,res,'register_company',400)
 })
 
 router.post('/login', async(req,res)=> {
     await authAction(req,res,'login',401)
+})
+
+router.post('/login_company', async(req,res)=> {
+    await authAction(req,res,'login_company',401)
 })
 
 router.get('/logout', async (req,res)=>{
@@ -35,16 +46,13 @@ async function authAction(req,res, action, httpErrorStatus) {
         let data;
         
         if ( action  === 'register' ) {
-            
-            if( formData.role === 'user' ) {
-                data = await register(formData.email, formData.password)
-                
-            } else if( formData.role === 'company' ) {
-                data = await registerAsCompany(formData.email, formData.password, formData.companyName, formData.desc, formData.foundationYear)
-                
-            }
+            data = await register(formData.email, formData.password)
+        } else if( action === 'register_company') {
+            data = await registerAsCompany(formData.email, formData.password, formData.companyName, formData.desc, formData.employees, formData.foundationYear)
+        } else if( action === 'login_company') {
+            data = await loginAsCompany(formData.email, formData.password)
         } else {
-            data = await login(formData.email, formData.password, formData.role)
+            data = await login(formData.email, formData.password)
         }
         
         res.json(data);
