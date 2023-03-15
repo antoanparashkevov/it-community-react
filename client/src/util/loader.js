3b4379a90933014edebe4810a33f3a0ee9a96815
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-15 22:17:55 +0200

    enhance the loader functionality and use route guards for some paths

--- client/src/util/loader.js
+++ client/src/util/loader.js
@@ -1,23 +1,69 @@
-import { json } from "react-router-dom";
+import { json, redirect, useSubmit } from "react-router-dom";
+import { getAuthToken } from "./auth";
 
 const host =  process.env.REACT_APP_DEFAULT_URL || 'http://localhost:3030';
 
-const loader = async (url, formatData) => {
+//TODO USE DEFER
+const loader = async (url, formatData, userType) => {
     
-    console.log('HOST', host)
-    console.log('URL', url)
+    // console.log('HOST', host)
+    // console.log('URL', url)
+    
+    const token = getAuthToken();
+    
+    const options = {
+        method: 'GET',
+        headers: {}
+    }
 
-    try {
-        const response = await fetch(host + url)
+    if ( token ) {
+        options.headers['X-Authorization'] = token;
+    }
+    
+    if ( userType ) {
+        
+        if ( userType.includes('company') ) {
+            const response = await fetch(host + '/userData', options)
 
-        if( response.ok === false ) {
+            const data = await response.json();
 
-            if( response.status === 401 ) {
-                //todo util func
+            if( data.userData.hasData === false ){
+                return redirect('/auth')
             }
-            const error = await response.json()
-            throw new Error(error.message)
+
+            if (data.userData.hasData && data.userData.roles.includes('company') === false) {
+                return redirect('/auth')
+            }
         }
+
+        if( userType.includes('admin') ) {
+            const response = await fetch(host + '/userData', options)
+
+            const data = await response.json();
+
+            if( data.userData.hasData === false ){
+                return redirect('/auth')
+            }
+
+            if (data.userData.hasData && data.userData.roles.includes('admin') === false) {
+                return redirect('/auth')
+            }
+        }
+       
+    }
+    
+    if( url ) {
+        try {
+            const response = await fetch(host + url, options)
+
+            if( response.ok === false ) {
+
+                if( response.status === 401 ) {
+                    //todo util func
+                }
+                const error = await response.json()
+                throw new Error(error.message)
+            }
 
             if( formatData ) {
                 const data = await response.json();
@@ -27,13 +73,16 @@ const loader = async (url, formatData) => {
                 return response;
             }
 
-    } catch ( error ) {
-        // throw new Response(JSON.stringify({ message: error.message || 'Something went wrong!' }))
-        //an alternative to
-        throw json( { message: error.message || 'Something went wrong!' }, {
-            status: 500
-        } ) 
+        } catch ( error ) {
+            // throw new Response(JSON.stringify({ message: error.message || 'Something went wrong!' }))
+            //an alternative to
+            throw json( { message: error.message || 'Something went wrong!' }, {
+                status: 500
+            } )
+        }
     }
+    
+    return null;
 }
 
 export default loader;
