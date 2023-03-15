199d767edc3875b2ef96cddfe078b134d2452e56
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-15 22:18:42 +0200

    Populate all needed fields when we access a specific job

--- server/services/jobService.js
+++ server/services/jobService.js
@@ -5,7 +5,7 @@ async function getAll() {
 }
 
 async function getById(id) {
-    return Job.findById(id).populate('category').populate('subCategory');
+    return Job.findById(id).populate('category').populate('subCategory').populate('companyId');
 }
 
 async function create(item) {
@@ -15,19 +15,21 @@ async function create(item) {
 // async function getJobRegistration(ownerId) {
 //     return Job.find({_ownerId: ownerId})
 // }
-
-async function update(itemId, modifiedItemData) {
-    let existing = await Job.findById(itemId)
-    console.log('Existing job item >>>')
-    //TODO update all entries wit the correct ones
-    // existing.firstName = modifiedItemData.firstName
-    // existing.lastName = modifiedItemData.lastName
-    // existing.description = modifiedItemData.description
-    // existing.hourlyRate = modifiedItemData.hourlyRate
-    // existing.skills = modifiedItemData.skills
-
-    // return await existing.save()//will return the saved Job Registration
-}
+//
+// async function update(itemId, modifiedItemData) {
+//     let existing = await Job.findById(itemId)
+//    
+//     console.log('Existing job item >>>', existing)
+//    
+//     //TODO update all entries wit the correct ones
+//     existing.firstName = modifiedItemData.firstName
+//     existing.lastName = modifiedItemData.lastName
+//     existing.description = modifiedItemData.description
+//     existing.hourlyRate = modifiedItemData.hourlyRate
+//     existing.skills = modifiedItemData.skills
+//
+//     return await existing.save()//will return the saved Job Registration
+// }
 
 async function deleteById(id) {
     return Job.findByIdAndRemove(id)
