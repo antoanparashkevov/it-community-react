26f543bc0d58a0cf6d342ae721dec591ac74d088
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-15 22:17:17 +0200

    Replace the hardcoded messages with the fetched ones

--- client/src/components/pages/messages/Messages.js
+++ client/src/components/pages/messages/Messages.js
@@ -1,4 +1,6 @@
 import styles from './Messages.module.scss';
+import React, { useEffect, useState } from "react";
+import { useRouteLoaderData } from "react-router-dom";
 
 //components
 import MessageItem from "../../messages/MessageItem";
@@ -7,42 +9,52 @@ import MessageItem from "../../messages/MessageItem";
 import { BaseCard } from "../../UI/BaseCard";
 import SeparationLine from "../../UI/SeparationLine";
 
+//hooks
+import useHttp from "../../../hooks/use-http";
+import BaseDialog from "../../UI/BaseDialog";
+import BaseSpinner from "../../UI/BaseSpinner";
+
 const Messages = () => {
-    let messages = [
-        {
-            fullName : 'Antoan Parashkevov',
-            email : 'antoanparashkevov@gmail.com',
-            message : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, aut autem culpa cumque eligendi et facilis fugit ipsam magni maiores nesciunt numquam quod quos recusandae sequi sint totam veritatis.Lorem ipsum dolor sit amet'
-        },
-        {
-            fullName : 'Antoan Parashkevov',
-            email : 'antoanparashkevov@gmail.com',
-            message : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, aut autem culpa cumque eligendi et facilis fugit ipsam magni maiores nesciunt numquam quod quos recusandae sequi sint totam veritatis.Lorem ipsum dolor sit amet'
-        },
-        {
-            fullName : 'Antoan Parashkevov',
-            email : 'antoanparashkevov@gmail.com',
-            message : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, aut autem culpa cumque eligendi et facilis fugit ipsam magni maiores nesciunt numquam quod quos recusandae sequi sint totam veritatis.Lorem ipsum dolor sit amet'
-        },
-        {
-            fullName : 'Antoan Parashkevov',
-            email : 'antoanparashkevov@gmail.com',
-            message : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, aut autem culpa cumque eligendi et facilis fugit ipsam magni maiores nesciunt numquam quod quos recusandae sequi sint totam veritatis.Lorem ipsum dolor sit amet'
-        },
-        
-    ]
+    const [messages, setMessages] = useState([])
+    
+    const { isLoading, error, resetError, sendRequest } = useHttp()
+    
+    const user = useRouteLoaderData('root');
+    
+    useEffect( () => {
+        fetchMessages()
+    }, [user]);
+    
+    const fetchMessages = async () => {
+      await sendRequest(`/applicationData/applications/${user.userData._id}`, 'GET', handleResponse);
+    }
+    
+    const handleResponse = (data) => {
+        setMessages(data.applicationItems);
+    }
+    
     return (
-        <section className={styles['messages_container']}>
-            <BaseCard className={styles['messages_wrapper']}>
-                <h1 className={styles['messages_title']}>Incoming messages</h1>
-                <SeparationLine />
-                <ul role='list' className={styles['messages_list']}>
-                    { messages.map((m, index) => {
-                        return <MessageItem message={ m } key={ index }/>
-                    }) }
-                </ul>
-            </BaseCard>
-        </section>
+        <React.Fragment>
+            {error &&
+                <BaseDialog fixed={false} show={!!error} title='Something went wrong with the authentication process' onCloseDialog={resetError}>
+                    {error}
+                </BaseDialog>
+            }
+            {isLoading && <BaseSpinner />}
+            <section className={styles['messages_container']}>
+                <BaseCard className={styles['messages_wrapper']}>
+                    <h1 className={styles['messages_title']}>Incoming messages</h1>
+                    <SeparationLine />
+                    <ul role='list' className={styles['messages_list']}>
+                        {/*TODO ADD NO MESSAGES*/}
+                        { messages.map((m, index) => {
+                            return <MessageItem message={ m } key={ index }/>
+                        }) }
+                    </ul>
+                </BaseCard>
+            </section>
+        </React.Fragment>
+       
     )
 }
 
