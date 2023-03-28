cbf20c56c13ce902a1ddfa43a00c2a3fce161926
“antoanparashkevov” <tonkata1505@gmail.com>
2023-03-28 17:41:31 +0300

    Defer the data in the Applying component

--- client/src/components/pages/applying/Applying.js
+++ client/src/components/pages/applying/Applying.js
@@ -1,5 +1,5 @@
-import { useRouteLoaderData } from "react-router-dom";
-import React, { useEffect } from "react";
+import { Await, useRouteLoaderData } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
 import styles from './Applying.module.css'
 
 //components
@@ -10,14 +10,22 @@ import { BaseCard } from "../../UI/BaseCard";
 import SeparationLine from "../../UI/SeparationLine";
 
 const Applying = () => {
-    const data = useRouteLoaderData('poster-details')
+    const { jobDetailsData } = useRouteLoaderData('poster-details')
     
     return (
-        <BaseCard className={styles['applying_container']}>
-            <h1 className={styles['applying_header']}>Send a message</h1>
-            <SeparationLine max-width='300px' />
-            <ApplyForm initialEmailValue={data.userData.userData.email ? data.userData.userData.email : ''} companyId={data.jobItem.company._id ? data.jobItem.company._id : ''}/>
-        </BaseCard>
+        <Suspense fallback={<p>Loading...</p>}>
+            <Await resolve={jobDetailsData}>
+                {
+                    (data) => (
+                        <BaseCard className={styles['applying_container']}>
+                            <h1 className={styles['applying_header']}>Send a message</h1>
+                            <SeparationLine max-width='300px' />
+                            <ApplyForm initialEmailValue={data['userData'].userData.email ? data['userData'].userData.email : ''} companyId={data['jobItem'].company._id ? data['jobItem'].company._id : ''}/>
+                        </BaseCard>
+                    )
+                }
+            </Await>
+        </Suspense>
     )
 }
 
